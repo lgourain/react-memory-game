54b4e38c023f53727ed86651f5d9202f81981062
Louis Gourain <louis-externe.gourain@enedis.fr>
2019-05-29 10:02:25 +0200

    adding sinon.js

--- src/Card.spec.js
+++ src/Card.spec.js
@@ -1,18 +1,20 @@
+import { expect } from 'chai'
 import React from 'react'
 import { shallow } from 'enzyme'
+import sinon from 'sinon'
 
 import Card from './Card'
 
 describe('<Card />', () => {
 
     it('should trigger its `onClick` prop when clicked', () => {
-        const onClick = jest.fn()
+        const onClick = sinon.spy()
         const wrapper = shallow(
           <Card card="😁" feedback="hidden" index={0} onClick={onClick} />
         )
     
         wrapper.simulate('click')
-        expect(onClick).toHaveBeenCalledWith(0)
+        expect(onClick).to.have.been.calledWith(0)
     })
-    
+
 })

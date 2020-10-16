9cefa8766560132292edb06f423c3cbed09b8b49
Louis Gourain <lgourain@wanadev.fr>
2020-10-16 21:55:09 +0200

    Updates

---
+++ precache-manifest.8f7316b91a8890c95f2c4f60ae0d48bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c76658181fd81e13b2be5a81ee915e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e268ef91e57fad652a8a",
+    "url": "/static/css/main.ea9970fd.chunk.css"
+  },
+  {
+    "revision": "e4d5c487b34b9a7f2c39",
+    "url": "/static/js/2.03d7b54d.chunk.js"
+  },
+  {
+    "revision": "e268ef91e57fad652a8a",
+    "url": "/static/js/main.3f43befd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

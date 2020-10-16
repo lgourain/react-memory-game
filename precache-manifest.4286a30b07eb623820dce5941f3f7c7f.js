833cb0bed5a2b191545e59cc8638b02d7a664787
Louis Gourain <lgourain@wanadev.fr>
2020-10-16 21:59:02 +0200

    Updates

---
+++ precache-manifest.4286a30b07eb623820dce5941f3f7c7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f775484d1d81a395b6de8fec1d6eaf7",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "e268ef91e57fad652a8a",
+    "url": "/react-memory-game/static/css/main.ea9970fd.chunk.css"
+  },
+  {
+    "revision": "e4d5c487b34b9a7f2c39",
+    "url": "/react-memory-game/static/js/2.03d7b54d.chunk.js"
+  },
+  {
+    "revision": "e268ef91e57fad652a8a",
+    "url": "/react-memory-game/static/js/main.3f43befd.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  }
+]);
